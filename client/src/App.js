31742157e5ef2476dc6b02460a448385ad7f3e35
Cynthia Godoy <cynthia@ck-interiors.com>
2022-07-20 22:11:25 -0600

    app.js router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -14,23 +14,14 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <>
+      <Router>
         <Navbar />
         <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
+          <Route path='/' element={<SearchBooks />} />
+          <Route path='/saved' element={<SavedBooks />} />
+          <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
         </Routes>
-      </>
+      </Router>
     </ApolloProvider>
   );
 }
